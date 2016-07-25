fdf076b483cac9df9e8fb884102f9e7092207bef
hc <hcchen@iir.csie.ncku.edu.tw>
2016-07-25 13:55:23 +0800

    adjust showing method

--- play.py
+++ play.py
@@ -1,22 +1,19 @@
 import world
 import character
-import scripts
 
 if __name__ == '__main__':
     dungeon = world.Dungeon(size=3)
-
-    scripts.hello()
     dungeon.view()
     while True:
         moves = dungeon.get_moves()
+        dungeon.view()
         while True:
             input_action = input('How? ')
             if input_action in moves:
                 break
         feedback = dungeon.action(input_action)
+        dungeon.view()
         if feedback == 0:
-            print('===============')
             break
-        print(feedback)
-        print('===============')
+    print('===============')
     print('Thank you for playing!')
--- world.py
+++ world.py
@@ -14,22 +14,25 @@ class Dungeon:
         for i in range(size-2):
             self.add_monster()
         self.add_player()
-    def add_player(self):
-        self.character = c_list.Player(self.init_pos())
-    def add_monster(self):
-        mon = c_list.Monster(self.init_pos())
-        self.monsters.append(mon)
+        self.descriptions = []
+        self.descriptions += scripts.hello()
+        self.show_map()
     # visualize
     def view(self):
+        """
         print("size:", self.size)
         print("exit:", self.exit_pos)
         print("your position:", self.character.pos)
         for mon in self.monsters:
             print("mon's position:", mon.pos)
         print('===============')
-        self.show_map()
+        """
+        for d in self.descriptions:
+            print(d)
+        self.descriptions = []
     # do action
     def get_moves(self):
+        self.monster_alert()
         self.show_moves()
         return ['w', 'a', 's', 'd', 'pass', 'p']
     def action(self, act):
@@ -39,27 +42,33 @@ class Dungeon:
             self.walk(self.character, act)
         self.show_map()
         self.monster_action()
-        self.monster_alert()
         return self.decision()
 
-
 # private method
+    #initialize
     def init_pos(self):
         while True:
             pos = [random.randint(0, self.size-1), random.randint(0, self.size-1)]
             if self.meet(pos) == 'nothing':
                 break
         return pos
-    # reaction
+    def add_player(self):
+        self.character = c_list.Player(self.init_pos())
+    def add_monster(self):
+        mon = c_list.Monster(self.init_pos())
+        self.monsters.append(mon)
+    # react
     def decision(self):
         meet = self.meet(self.character.pos)
         if meet == 'exit':
-            scripts.escape()
+            self.descriptions += scripts.escape()
             return 0
         elif meet == 'monster':
-            scripts.meet_monster()
+            self.descriptions += scripts.meet_monster()
             return 0
+        self.descriptions += ['nothing happens']
         return 'nothing happens'
+    # func
     def meet(self, pos):
         if pos == self.exit_pos:
             return 'exit'
@@ -74,32 +83,24 @@ class Dungeon:
     def walk(self, role, act):
         if act == 'w':
             if role.pos[1] == 0:
-                scripts.there_is_wall()
+                self.descriptions += scripts.there_is_wall()
             else:
                 role.pos[1] -= 1
         elif act == 'a':
             if role.pos[0] == 0:
-                scripts.there_is_wall()
+                self.descriptions += scripts.there_is_wall()
             else:
                 role.pos[0] -= 1
         elif act == 's':
             if role.pos[1] == self.size-1:
-                scripts.there_is_wall()
+                self.descriptions += scripts.there_is_wall()
             else:
                 role.pos[1] += 1
         elif act == 'd':
             if role.pos[0] == self.size-1:
-                scripts.there_is_wall()
+                self.descriptions += scripts.there_is_wall()
             else:
                 role.pos[0] += 1
-    def monster_alert(self):
-        x = self.character.pos[0]
-        y = self.character.pos[1]
-        around = [[x+1, y], [x-1, y], [x, y+1], [x, y-1]]
-        for mon in self.monsters:
-            if mon.pos in around:
-                scripts.monster_alert()
-                print('===============')
     # monster move
     def monster_action(self):
         for mon in self.monsters:
@@ -123,17 +124,29 @@ class Dungeon:
         self.walk(mon, moves[random.randint(0, len(moves)-1)])
     # show
     def show_map(self):
+        string = '===============\n'
         for y in range(self.size):
             for x in range(self.size):
                 if [x, y] == self.character.pos:
-                    print('[★]', end="")
+                    string += '[★]'
                 else:
-                    print('[  ]', end="")
-            print('')
-        print('===============')
+                    string += '[  ]'
+            string += '\n'
+        string += '==============='
+        self.descriptions += [string]
     def show_moves(self):
-        print('\t↑')
-        print('\tw')
-        print('←a\t\td→')
-        print('\ts')
-        print('\t↓\t\tor pass')
+        string = '===============\n'
+        string += '\t↑\n'
+        string += '\tw\n'
+        string += '←a\t\td→\n'
+        string += '\ts\n'
+        string += '\t↓\t\tor pass\n'
+        string += '==============='
+        self.descriptions += [string]
+    def monster_alert(self):
+        x = self.character.pos[0]
+        y = self.character.pos[1]
+        around = [[x+1, y], [x-1, y], [x, y+1], [x, y-1]]
+        for mon in self.monsters:
+            if mon.pos in around:
+                self.descriptions += scripts.monster_alert()
